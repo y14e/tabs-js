8cb794e8f69ccf89a6af06fb2f7cf15986b4d58c
Yusuke Kamiyamane <y@y14e.com>
2025-09-23 20:58:23 +0900

    Update tabs.ts

--- tabs.ts
+++ tabs.ts
@@ -159,7 +159,7 @@ export default class Tabs {
   }
 
   private getActiveElement(): HTMLElement | null {
-    let active: Element | null = document.activeElement;
+    let active = document.activeElement;
     while (active && active.shadowRoot?.activeElement) {
       active = active.shadowRoot.activeElement;
     }
